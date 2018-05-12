d2d740d967db2e7e15048adce21791f960c45924
Maximilian Albert <maximilian.albert@flowminder.org>
2018-05-12 14:45:27 +0100

    Update configurations

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -3,13 +3,14 @@ class BaseConfig:
     Base configuration
     """
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(BaseConfig):
     """
     Development Configuration
     """
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class TestingConfig(BaseConfig):
@@ -17,10 +18,11 @@ class TestingConfig(BaseConfig):
     Testing configuration
     """
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 
 class ProductionConfig(BaseConfig):
     """
     Production configuration
     """
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
