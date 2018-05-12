29ecf67b58067dc4f93040fedf7895dbc7814bc2
Maximilian Albert <maximilian.albert@flowminder.org>
2018-05-12 14:18:03 +0100

    Minimal app and docker config for dev environment

---
+++ services/users/project/config.py
@@ -0,0 +1,26 @@
+class BaseConfig:
+    """
+    Base configuration
+    """
+    TESTING = False
+
+
+class DevelopmentConfig(BaseConfig):
+    """
+    Development Configuration
+    """
+    pass
+
+
+class TestingConfig(BaseConfig):
+    """
+    Testing configuration
+    """
+    TESTING = True
+
+
+class ProductionConfig(BaseConfig):
+    """
+    Production configuration
+    """
+    pass
