2325f40cd5becf4ebe41b2d1360b94733005b4e2
Maximilian Albert <maximilian.albert@flowminder.org>
2018-05-13 15:35:46 +0100

    Add route /users

--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -6,6 +6,13 @@ from project.tests.base import BaseTestCase
 from project.api.models import User
 
 
+def add_user(username, email):
+    user = User(username=username, email=email)
+    db.session.add(user)
+    db.session.commit()
+    return user
+
+
 class TestUserService(BaseTestCase):
     """
     Test for the Users Service
@@ -100,9 +107,7 @@ class TestUserService(BaseTestCase):
         """
         Ensure single user behaves correctly.
         """
-        user = User(username='michael', email='michael@mherman.org')
-        db.session.add(user)
-        db.session.commit()
+        user = add_user('michael', 'michael@mherman.org')
         with self.client:
             response = self.client.get(f'/users/{user.id}')
             data = json.loads(response.data.decode())
@@ -132,6 +137,23 @@ class TestUserService(BaseTestCase):
             self.assertEqual(response.status_code, 404)
             self.assertIn('User does not exist', data['message'])
             self.assertIn('fail', data['status'])
+
+    def test_all_users(self):
+        """
+        Ensure get all users behaves correctly.
+        """
+        add_user('michael', 'michael@mherman.org')
+        add_user('fletcher', 'fletcher@notreal.com')
+        with self.client:
+            response = self.client.get('/users')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(len(data['data']['users']), 2)
+            self.assertIn('michael', data['data']['users'][0]['username'])
+            self.assertIn('michael@mherman.org', data['data']['users'][0]['email'])
+            self.assertIn('fletcher', data['data']['users'][1]['username'])
+            self.assertIn('fletcher@notreal.com', data['data']['users'][1]['email'])
+            self.assertIn('success', data['status'])
 
 
 if __name__ == '__main__':
