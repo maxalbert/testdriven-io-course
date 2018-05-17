f67836313f536f3d85c6bde47c3c8588084816a2
Maximilian Albert <maximilian.albert@flowminder.org>
2018-05-17 19:31:17 +0100

    Main App component

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const App = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4">
+          <br/>
+          <h1>All Users</h1>
+          <hr/><br/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
