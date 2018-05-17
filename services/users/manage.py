82fc0670dfaa308f24788462881bf39cfb27e937
Maximilian Albert <maximilian.albert@flowminder.org>
2018-05-17 18:46:18 +0100

    Add cli command to run tests with code coverage

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,7 +1,18 @@
+import coverage
 import unittest
 from flask.cli import FlaskGroup
 from project import create_app, db
 from project.api.models import User
+
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py',
+    ]
+)
+COV.start()
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
@@ -31,6 +42,24 @@ def test():
     tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
+        return 0
+    return 1
+
+
+@cli.command()
+def cov():
+    """
+    Runs the unit tests with code coverage
+    """
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
         return 0
     return 1
 
