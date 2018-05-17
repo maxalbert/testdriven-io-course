a18c30d9c9b31c05973fb115a6a3a6a41cb4e019
Maximilian Albert <maximilian.albert@flowminder.org>
2018-05-17 19:55:56 +0100

    Enable cross-origin resource sharing

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -9,6 +10,8 @@ toolbar = DebugToolbarExtension()
 
 def create_app(script_info=None):
     app = Flask(__name__)
+
+    CORS(app)
 
     app_settings = os.getenv('APP_SETTINGS')
     app.config.from_object(app_settings)
