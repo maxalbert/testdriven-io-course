901dc7dbc700cdceb1d60ef05cb5723ea3550e47
Maximilian Albert <maximilian.albert@flowminder.org>
2018-05-27 13:49:25 +0100

    Add seed_db command

--- services/users/manage.py
+++ services/users/manage.py
@@ -17,6 +17,14 @@ def recreate_db():
 
 
 @cli.command()
+def seed_db():
+    """Seeds the database."""
+    db.session.add(User(username='michael', email="hermanmu@gmail.com"))
+    db.session.add(User(username='michaelherman', email="michael@mherman.org"))
+    db.session.commit()
+
+
+@cli.command()
 def test():
     """
     Runs the tests without code coverage
