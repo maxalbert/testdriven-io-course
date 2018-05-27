d4e933f5136bee2beb7eb907ea02e4638ee1bb5f
Maximilian Albert <maximilian.albert@flowminder.org>
2018-05-27 21:59:34 +0100

    Add flask-debugtoolbar

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -8,6 +8,8 @@ class BaseConfig:
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'my_precious'
+    DEBUG_TB_ENABLED = False
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
 
 
 class DevelopmentConfig(BaseConfig):
@@ -15,6 +17,7 @@ class DevelopmentConfig(BaseConfig):
     Development configuration
     """
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    DEBUG_TB_ENABLED = True
 
 
 class TestingConfig(BaseConfig):
