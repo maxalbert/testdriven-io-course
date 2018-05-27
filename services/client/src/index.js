6be95040a940b6f48f5390763460fe370d42474e
Maximilian Albert <maximilian.albert@flowminder.org>
2018-05-27 22:29:25 +0100

    Add UsersList component

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import UsersList from './components/UsersList';
+
 
 class App extends Component {
   constructor() {
@@ -28,17 +30,7 @@ class App extends Component {
             <br/>
             <h1>All Users</h1>
             <hr/><br/>
-            {
-              this.state.users.map((user) => {
-                return (
-                  <h4
-                    key={user.id}
-                    className="card card-body bg-light"
-                  >{ user.username }
-                  </h4>
-                )
-              })
-            }
+              <UsersList users={this.state.users} />
           </div>
         </div>
       </div>
